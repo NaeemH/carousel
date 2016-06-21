d7a4a93f7801adb2751b73d83f8f73f0d010f375
Naeem <naeem@rrcs-198-179-74-14.nyc.biz.rr.com>
2016-06-21 18:19:32 -0400

    first iteration, will add images later

--- carousel/ViewController.swift
+++ carousel/ViewController.swift
@@ -8,12 +8,66 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, iCarouselDelegate, iCarouselDataSource {
 
+    var numbers = [Int]()
+    
+    
+    /*
+    var gameImages: UIImage = [
+        UIImage(named: "zelda")!,
+        UIImage(named: "pokemon")!,
+        UIImage(named: "mario")!,
+        UIImage(named: "rdr")!,
+        UIImage(named: "cod")!,
+        UIImage(named: "minecraft")!
+    ]
+    */
+    
+    @IBOutlet var carouselView: iCarousel!
+    
+    //Where numbers will load as soon as app loads
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        numbers = [1,2,3,4,5,6]
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //carouselView.type = .coverFlow
+        carouselView.type = .invertedTimeMachine
     }
+    
+    func numberOfItems(in carousel: iCarousel) -> Int {
+        
+     return numbers.count
+    }
+    
+    func carousel(_ carousel: iCarousel, viewForItemAt index: Int, reusing view: UIView?) -> UIView {
+        
+        let tempView = UIView(frame: CGRect (x: 0, y: 0, width: 200, height: 200))
+        //tempView.backgroundColor = UIColor.black()
+        
+        //HOW TO ADD SOMETHING TO THE VIEW
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+        button.setTitle("\(index)", for: .application) //, for: .normal
+        button.backgroundColor = UIColor.blue()
+        tempView.addSubview(button)
+        
+        return tempView
+    }
+    
+    func carousel(_ carousel: iCarousel, valueFor option: iCarouselOption, withDefault value: CGFloat) -> CGFloat {
+        //options to edit carousels spacing
+        if option == iCarouselOption.spacing{
+            
+            return value * 1.2
+        }
+        return value
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
